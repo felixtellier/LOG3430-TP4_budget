72e8c1dd13b0dbf4117b0d3a86352de61b876b04
Félix Tellier <felix.tellier@icloud.com>
2024-03-11 19:49:22 -0400

    Modified myscript

--- myscript.py
+++ myscript.py
@@ -1,11 +1,11 @@
 import os
 
+
 def main():
     if (os.system("python manage.py test") != 0):
         badCommit = os.popen("git rev-parse HEAD").read().strip()
         os.system(f"git bisect start {badCommit} e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
         os.system("git bisect run python manage.py test")
-        os.system("git bisect reset")
 
 
 if __name__ == '__main__':
