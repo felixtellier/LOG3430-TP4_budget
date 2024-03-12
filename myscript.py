fef7fdf1f30ae3d89d3e7a81bc607f0962a581ea
Félix Tellier <felix.tellier@icloud.com>
2024-03-12 15:06:47 -0400

    Fix script

--- myscript.py
+++ myscript.py
@@ -6,9 +6,9 @@ def main():
     if (os.system("python manage.py test") != 0):
         badCommit = os.popen("git rev-parse HEAD").read().strip()
         os.system(f"git bisect start {badCommit} e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c")
-        firstBadCommit = os.popen("git bisect run python manage.py test").read().strip()
+        os.system("git bisect run python manage.py test")
         os.system("git bisect reset")
-        sys.exit(firstBadCommit)
+        sys.exit(1)
     sys.exit(0)
 
 
